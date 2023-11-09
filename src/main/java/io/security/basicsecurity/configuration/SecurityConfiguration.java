dffb74098192194842f9440e0a816311f2485872
DaeHyeonHong <fholng@outlook.com>
2023-11-09 22:54:16 +0900

    [:sparkle:] [feat] Security 다중 설정

--- src/main/java/io/security/basicsecurity/configuration/SecurityConfiguration.java
+++ src/main/java/io/security/basicsecurity/configuration/SecurityConfiguration.java
@@ -4,17 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.RequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Slf4j
 @Configuration
@@ -23,107 +18,32 @@ public class SecurityConfiguration {
     private final UserDetailsService userDetailsService;
 
     @Bean
-    public static UserDetailsManager userDetailsManager() {
-        final UserDetails user = User.builder()
-                .username("user")
-                .password("{noop}1111")
-                .roles("USER")
-                .build();
+    @Order(0)
+    public SecurityFilterChain adminFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/admin/pay")
+                .authorizeHttpRequests(request ->
+                        request.anyRequest().hasRole("ADMIN"));
 
-        final UserDetails sys = User.builder()
-                .username("sys")
-                .password("{noop}1111")
-                .roles("SYS")
-                .build();
-
-        final UserDetails admin = User.builder()
-                .username("admin")
-                .password("{noop}1111")
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, sys, admin);
+        return http.build();
     }
 
     @Bean
-    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+    @Order(1)
+    public SecurityFilterChain systemFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
-                        authorizationManagerRequestMatcherRegistry
-                                .requestMatchers("/login").permitAll()
-                                .requestMatchers("/user").hasRole("USER")
-                                .requestMatchers("/admin/pay").hasRole("ADMIN")
-                                .requestMatchers("/admin/**").hasAnyRole("ADMIN", "SYS")
-                                .anyRequest().authenticated()
-                );
-
-        http.csrf(AbstractHttpConfigurer::disable);
-
-        http
-                .formLogin(
-                        httpSecurityFormLoginConfigurer ->
-                                httpSecurityFormLoginConfigurer
-                                        .defaultSuccessUrl("/")
-                                        .failureUrl("/login")
-                                        .usernameParameter("userId")
-                                        .passwordParameter("passwd")
-                                        .loginProcessingUrl("/login_proc")
-                                        .successHandler((request, response, authentication) -> {
-                                            log.info("authentication: {}", authentication.getName());
-                                            final RequestCache requestCache = new HttpSessionRequestCache();
-                                            final SavedRequest savedRequest = requestCache.getRequest(request, response);
-                                            final String redirectUrl = savedRequest.getRedirectUrl();
-                                            response.sendRedirect(redirectUrl);
-                                        })
-                                        .failureHandler(
-                                                (request, response, exception) -> {
-                                                    log.error("exception: {}", exception.getMessage());
-                                                    response.sendRedirect("/login");
-                                                }
-                                        )
-                                        .permitAll()
-                );
-
-        http
-                .logout(
-                        httpSecurityLogoutConfigurer ->
-                                httpSecurityLogoutConfigurer
-                                        .logoutUrl("/logout")
-                                        .logoutSuccessUrl("/login")
-                                        .addLogoutHandler((request, response, authentication) -> request.getSession().invalidate())
-                                        .logoutSuccessHandler((request, response, authentication) -> response.sendRedirect("/login"))
-                                        .deleteCookies("remember-me")
-                                        .permitAll()
-                );
-
-        http
-                .rememberMe(
-                        httpSecurityRememberMeConfigurer ->
-                                httpSecurityRememberMeConfigurer
-                                        .rememberMeParameter("remember")
-                                        .tokenValiditySeconds(3_600)
-                                        .userDetailsService(this.userDetailsService)
-                );
-
-        http
-                .sessionManagement(
-                        httpSecuritySessionManagementConfigurer ->
-                                httpSecuritySessionManagementConfigurer
-                                        .invalidSessionUrl("/invalid")
-                                        .sessionFixation().changeSessionId()
-//                                        .maximumSessions(1)
-//                                        .maxSessionsPreventsLogin(true)
-//                                        .expiredUrl("/expired")
-                );
-
-        http
-                .exceptionHandling(
-                        httpSecurityExceptionHandlingConfigurer ->
-                                httpSecurityExceptionHandlingConfigurer
-//                                        .authenticationEntryPoint((request, response, authException) -> response.sendRedirect("/login"))
-                                        .accessDeniedHandler((request, response, accessDeniedException) -> response.sendRedirect("/denied"))
-                );
-
+                .securityMatcher("/admin/**")
+                .authorizeHttpRequests(request -> request
+                        .anyRequest().hasAnyRole("SYS", "ADMIN"));
         return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(antMatcher("/user/**")).hasRole("USER")
+                        .anyRequest().authenticated()).build();
     }
 }
